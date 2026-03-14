2d79c7e88f0013bd6cbcd96a28d965a8d9a28165
melihaecedelier <ecedelier12@gmail.com>
2026-03-14 14:12:07 +0300

    Adding product model

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController =TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -31,8 +33,43 @@ class _HomeScreenState extends State<HomeScreen> {
                 icon: Icon(Icons.shopping_bag_outlined))
               ],
             ),
+            SizedBox(height: 8),
+            Text(
+              "Find your perfect device.",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 14,),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xfff5f5f5),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: "Search Products",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.search, color: Colors.grey,),
+                  contentPadding: EdgeInsets.symmetric(vertical: 14)
+                  
+                ),
+              ),
+            ),
 
-            
+            SizedBox(height: 16,),
+
+            Container(
+              width: double.infinity,
+              height: 80.0,
+              decoration:BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(image: NetworkImage("https://wantapi.com/assets/banner.png"
+                ),
+                fit: BoxFit.fitWidth
+                ),
+              ) ,
+            ),
           ],
         ),
       ),
